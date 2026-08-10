1824849d7d98d4f0064a0dda04085c3d01fc94a3
Skillcheese <34143022+Skillcheese@users.noreply.github.com>
2026-08-10 10:34:35 -0500

    chore(main): release 3.12.0 (#23)
    * chore(main): release 3.12.0
    
    * chore: sync version and format release notes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -136,7 +136,7 @@ if (isCombatSimulatorPage()) {
     const cheezashaRoot = targetWindow.Cheezasha || {};
     targetWindow.Cheezasha = cheezashaRoot;
 
-    cheezashaRoot.version = '3.11.0';
+    cheezashaRoot.version = '3.12.0';
 
     cheezashaRoot.features = {
         list: () => config.getFeaturesByCategory(),
